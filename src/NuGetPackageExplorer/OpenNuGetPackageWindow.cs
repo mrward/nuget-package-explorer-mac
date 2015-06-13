ec8819e418b9f80e14a96ca628839a5cfc416a4d
Matt Ward <ward.matt@gmail.com>
2015-06-13 19:56:51 +0100

    Enter key now closes the open packages modal dialog.
    Xamarin.Mac does not seem to handle having the key equivalent
    for the Close button set in the .xib with Xcode so instead the
    key equivalent is set to "\r" directly in code for the Close button.

--- src/NuGetPackageExplorer/OpenNuGetPackageWindow.cs
+++ src/NuGetPackageExplorer/OpenNuGetPackageWindow.cs
@@ -19,6 +19,8 @@ namespace NuGetPackageExplorer
 		public override void AwakeFromNib ()
 		{
 			base.AwakeFromNib ();
+
+			closeButton.KeyEquivalent = "\r";
 		}
 	}
 }
--- src/NuGetPackageExplorer/OpenNuGetPackageWindow.designer.cs
+++ src/NuGetPackageExplorer/OpenNuGetPackageWindow.designer.cs
@@ -1,7 +1,26 @@
-﻿namespace NuGetPackageExplorer
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace NuGetPackageExplorer
 {
-	[global::Foundation.Register ("OpenNuGetPackageWindow")]
-	public partial class OpenNuGetPackageWindow
+	[Register ("OpenNuGetPackageWindow")]
+	partial class OpenNuGetPackageWindow
 	{
+		[Outlet]
+		AppKit.NSButtonCell closeButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (closeButton != null) {
+				closeButton.Dispose ();
+				closeButton = null;
+			}
+		}
 	}
 }
